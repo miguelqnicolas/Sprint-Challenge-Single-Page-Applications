4aafafce83d8607b4e315fb7a612bd41d55aa783
Miguel Nicolas <miguelqnicolas@gmail.com>
2019-11-15 12:22:34 -0500

    cards completed, organized, will now style and complete search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
-import Header from "./components/Header.js";
-
+import CharacterList from "./components/CharacterList.js";
+import {Link, Route} from 'react-router-dom'
 
 export default function App() {
   return (
     <main>
-      <Header/>
+      <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <h1>Welcome to the ultimate fan site!</h1>
+      {/* <Route exact path='/' component={WelcomePage}/> */}
+      
+      <Link to='/characters'><button>Characters</button></Link>
+      <Route path='/characters' component={CharacterList}/>
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,35 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import {Link} from 'react-router-dom';
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character/')
+      .then(response => {
+        setCharacters(response.data.results);
+      })
+      .catch(error => {
+        console.log(error);
+      })
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+    <>
+      <Link to='/'><button>Back Home</button></Link>
+      <section className="character-list">
+        {characters.map((iteration, index) => (
+          <div key={index}>
+            <h2>{iteration.name}</h2>
+            <p>{iteration.status}</p>
+            <p>{iteration.species}</p>
+            <p>{iteration.type}</p>
+          </div>
+        ))}
+      </section>
+    </>
   );
 }
